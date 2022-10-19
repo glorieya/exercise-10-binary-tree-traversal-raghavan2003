5ea940f918ed5371d24bba19fd46e38c57d065b7
raghavan2003 <109953359+raghavan2003@users.noreply.github.com>
2022-10-19 17:20:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 class BinaryTreeNode:
+    """
+    Binary Tree has 3 attributes
+    data -> to store the data
+    left_child -> to set element to left
+    right_child -> to set element to right
+    """
     def __init__(self, data):
         self.data = data
         self.left_child = None
@@ -11,20 +17,55 @@ def insert(root, new_value) -> BinaryTreeNode:
         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
         Finally, return the root.
         """
-    # Write your code here
+    if (root == None):
+        root = BinaryTreeNode(new_value)
+        return root
+    else:
+        if root.data > new_value:
+            if root.left_child is None:
+                new_node = BinaryTreeNode(new_value)
+                root.left_child = new_node
+            else:
+                insert(root.left_child,new_value)
 
+        else:
+            if root.right_child is None:
+                new_node = BinaryTreeNode(new_value)
+                root.right_child = new_node
+            else:
+                insert(root.right_child,new_value)
 
 def inorder(root) -> None:
-    # Write your code here
+    """
+    to print in the order
+    left mid right
+    """
+    if root:
+        inorder(root.left_child)
+        print(root.data, end = " ")
+        inorder(root.right_child)
 
 
 def preorder(root) -> None:
-    # Write your code here
+    """
+    to print in the order
+    mid left right
+    """
+    if root:
+        print(root.data, end = " ")
+        preorder(root.left_child)
+        preorder(root.right_child)
 
 
 def postorder(root) -> None:
-    # Write your code here
-
+    """
+    to print in the order
+    left right left
+    """
+    if root:
+        postorder(root.left_child)
+        postorder(root.right_child)
+        print(root.data, end = " ")
 
 # Do not change the following code
 input_data = input()
