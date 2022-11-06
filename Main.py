4b3861fa704e71f02954e4b4d1db2abdb5ac3a02
glorieya <110552604+glorieya@users.noreply.github.com>
2022-11-06 22:23:09 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,4 @@
 class BinaryTreeNode:
-    """
-    Binary Tree has 3 attributes
-    data -> to store the data
-    left_child -> to set element to left
-    right_child -> to set element to right
-    """
     def __init__(self, data):
         self.data = data
         self.left_child = None
@@ -17,55 +11,20 @@ def insert(root, new_value) -> BinaryTreeNode:
         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
         Finally, return the root.
         """
-    if (root == None):
-        root = BinaryTreeNode(new_value)
-        return root
-    else:
-        if root.data > new_value:
-            if root.left_child is None:
-                new_node = BinaryTreeNode(new_value)
-                root.left_child = new_node
-            else:
-                insert(root.left_child,new_value)
+    # Write your code here
 
-        else:
-            if root.right_child is None:
-                new_node = BinaryTreeNode(new_value)
-                root.right_child = new_node
-            else:
-                insert(root.right_child,new_value)
 
 def inorder(root) -> None:
-    """
-    to print in the order
-    left mid right
-    """
-    if root:
-        inorder(root.left_child)
-        print(root.data, end = " ")
-        inorder(root.right_child)
+    # Write your code here
 
 
 def preorder(root) -> None:
-    """
-    to print in the order
-    mid left right
-    """
-    if root:
-        print(root.data, end = " ")
-        preorder(root.left_child)
-        preorder(root.right_child)
+    # Write your code here
 
 
 def postorder(root) -> None:
-    """
-    to print in the order
-    left right left
-    """
-    if root:
-        postorder(root.left_child)
-        postorder(root.right_child)
-        print(root.data, end = " ")
+    # Write your code here
+
 
 # Do not change the following code
 input_data = input()
@@ -81,4 +40,16 @@ inorder(root)
 print()
 preorder(root)
 print()
-postorder(root)
+
+
+
+
+
+
+
+
+
+
+
+
+
